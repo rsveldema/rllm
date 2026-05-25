d7bf90d7d3678d9bbf930ddf070a1642e850b4d3
Ronald Veldema <rsveldema@gmail.com>
2026-05-25 15:34:31 +0200

    feat: Try incr longer training

--- learn.sh
+++ learn.sh
@@ -3,6 +3,11 @@
 #    -o models/after_training.json \
 #     --filter iuring --method window:3 --epochs 5
 
+# ./build/rllm --train -i models/start.json \
+#     -o models/after_training.json \
+#      --filter simple --method three_tok --epochs 50
+#
+
 ./build/rllm --train -i models/start.json \
     -o models/after_training.json \
-     --filter simple --method three_tok --epochs 50
+     --filter simple --method increasingly_longer --epochs 50
